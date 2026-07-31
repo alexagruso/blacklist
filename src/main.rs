650ddb7d8b70e9f6a582304a2af358289aafc757
Alex <agrusoalexander@gmail.com>
2026-07-30 19:26:56 -0500

    character controller movement system and mock player entity

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,36 @@
-use bevy::{log::LogPlugin, prelude::*};
+use avian2d::math::Scalar;
+use bevy::{app::PluginGroupBuilder, log::LogPlugin, prelude::*};
 
 fn main() {
     App::new()
-        // TODO: at some point I'd like to remove the default plugin group and add manually add all necessary plugins.
+        // TODO: at some point I'd like to remove the default plugin group and manually add all necessary plugins.
         .add_plugins(DefaultPlugins.build().disable::<LogPlugin>())
         .add_plugins((
-            GamePlugin,
+            GamePlugins,
             #[cfg(debug_assertions)]
             GameDebugPlugin,
         ))
         .run();
 }
 
-struct GamePlugin;
+struct GamePlugins;
 
-impl Plugin for GamePlugin {
+impl PluginGroup for GamePlugins {
+    fn build(self) -> PluginGroupBuilder {
+        PluginGroupBuilder::start::<Self>()
+            .add(SpawnPlugin)
+            .add(PlayerPlugin)
+            .add(CharacterControllerPlugin)
+    }
+}
+
+struct SpawnPlugin;
+
+impl Plugin for SpawnPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, (spawn_camera, spawn_player));
     }
 }
-
-#[derive(Component)]
-#[require(Transform)]
-struct Player;
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
@@ -34,10 +42,139 @@ fn spawn_player(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn((
-        Player,
+        Player::default(),
+        Character,
         Mesh2d(meshes.add(Rectangle::new(50.0, 50.0))),
         MeshMaterial2d(materials.add(Color::srgb(0.25, 0.9, 0.35))),
     ));
+}
+
+#[derive(Component)]
+#[require(Transform)]
+struct Player {
+    movement_speed: f32,
+    rotation_speed: f32,
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Self {
+            movement_speed: 150.0,
+            rotation_speed: 180.0,
+        }
+    }
+}
+
+struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, player_input);
+    }
+}
+
+fn player_input(
+    time: Res<Time<Fixed>>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    player: Query<(&Player, Entity)>,
+    mut character_movement: MessageWriter<CharacterMovement>,
+) {
+    let (player, player_entity) = player.single().expect("multiple player entities exist");
+    let delta_time = time.delta_secs();
+
+    // We use integers here to avoid floating point comparison issues below
+    let mut velocity = IVec2::ZERO;
+    let mut angle = 0;
+
+    if keyboard.pressed(KeyCode::KeyA) {
+        velocity.x -= 1;
+    }
+    if keyboard.pressed(KeyCode::KeyD) {
+        velocity.x += 1;
+    }
+
+    if keyboard.pressed(KeyCode::KeyW) {
+        velocity.y += 1;
+    }
+    if keyboard.pressed(KeyCode::KeyS) {
+        velocity.y -= 1;
+    }
+
+    if velocity != IVec2::ZERO {
+        let velocity = vec2(velocity.x as f32, velocity.y as f32).normalize_or_zero()
+            * player.movement_speed
+            * delta_time;
+        character_movement.write(CharacterMovement::translation(player_entity, velocity));
+    }
+
+    if keyboard.pressed(KeyCode::KeyQ) {
+        angle -= 1;
+    }
+    if keyboard.pressed(KeyCode::KeyE) {
+        angle += 1;
+    }
+
+    if angle != 0 {
+        let angle = (angle as Scalar).to_radians() * player.rotation_speed * delta_time;
+        character_movement.write(CharacterMovement::rotation(player_entity, angle));
+    }
+}
+
+/// Marker for any entity that responds to character controller messages
+#[derive(Component)]
+struct Character;
+
+enum MovementType {
+    Translation(Vec2),
+    Rotation(Scalar),
+}
+
+#[derive(Message)]
+struct CharacterMovement {
+    entity: Entity,
+    movement_type: MovementType,
+}
+
+impl CharacterMovement {
+    fn translation(entity: Entity, vec: Vec2) -> Self {
+        Self {
+            entity,
+            movement_type: MovementType::Translation(vec),
+        }
+    }
+
+    fn rotation(entity: Entity, angle: Scalar) -> Self {
+        Self {
+            entity,
+            movement_type: MovementType::Rotation(angle),
+        }
+    }
+}
+
+struct CharacterControllerPlugin;
+
+impl Plugin for CharacterControllerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_message::<CharacterMovement>()
+            .add_systems(FixedUpdate, character_movement);
+    }
+}
+
+fn character_movement(
+    mut characters: Query<&mut Transform, With<Character>>,
+    mut character_movement: MessageReader<CharacterMovement>,
+) {
+    for message in character_movement.read() {
+        let mut transform = characters.get_mut(message.entity).expect(&format!(
+            "character movement message sent for non-existent entity.\nID: {}",
+            message.entity
+        ));
+
+        match message.movement_type {
+            MovementType::Translation(vec) => transform.translation += vec.extend(0.0),
+            MovementType::Rotation(angle) => transform.rotate_z(angle),
+        }
+    }
 }
 
 struct GameDebugPlugin;
